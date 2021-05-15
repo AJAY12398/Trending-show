661c8102d135166b65f31b9ed632c9fb3401481d
AJAY12398 <ajay.mandaviya03@gmail.com>
2021-05-15 17:03:15 +0530

    Change Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import './Header.css'
 const Header = () => {
     return (
-    <span className = "header" onClick = {()=> window.scroll(0,0)}>Entertainment Hub</span>
+    <span className = "header" onClick = {()=> window.scroll(0,0)}>Trending Show</span>
     )
     
 }

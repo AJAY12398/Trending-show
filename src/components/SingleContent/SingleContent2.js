5ca5d14adda9ea7d735d33887b68da272408d9d1
AJAY12398 <ajay.mandaviya03@gmail.com>
2021-05-15 19:18:17 +0530

    change button name

--- src/components/SingleContent/SingleContent2.js
+++ src/components/SingleContent/SingleContent2.js
@@ -2,13 +2,12 @@ import { Badge } from '@material-ui/core'
 import {img_300  , unavailable} from  '../../Config/config';
 import Button from '@material-ui/core/Button';
 import './SingleContent.css'
-import {Link} from 'react-router-dom';
 const SingleContent2 = ({   id , release_date, media_type ,first_air_date  ,  title , name ,  poster_path , overview , vote_average , reload, setReload = f => f}) => {
 
     const deleteWatchList = () => {
         const getWatchList = JSON.parse(localStorage.getItem("watchlist" || "[]"));
        let watchList = getWatchList.filter((movie) => {
-            return  movie.id != id;
+            return  movie.id !== id;
         })
         localStorage.setItem("watchlist", JSON.stringify(watchList))
         setReload(!reload)
@@ -23,10 +22,7 @@ const SingleContent2 = ({   id , release_date, media_type ,first_air_date  ,  ti
                 <span className = "typeof">{media_type === "tv"? "Tv Series": "Movie"}
                 <span className = "date">{release_date || first_air_date}</span>
                 </span>
-             {/* <Link  style = {{textDecoration: "none"}}  to = {`/watchlist/${id}`}>
-                </Link> */}
-                <Button variant="contained"  onClick = {deleteWatchList} className = "btn" color="primary">Delete Button</Button>
-                {/* <Link to = {`watchlist${id}`}>Add to  {id}</Link> */}
+                <Button variant="contained"  onClick = {deleteWatchList} className = "btn" color="primary">Remove from List</Button>
         </div>
         
     )
